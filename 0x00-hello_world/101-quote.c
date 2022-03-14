3cdb6f6b7b1ac8bef0368320d71f24b1a2f79798
Adebare <adebaresulaimon04@gmail.com>
2022-03-13 18:31:29 -0700

    hello, World!

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdib.h>
+
+/**
+ * main - Entry point
+ * Return - Always 1
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpor, 2015-10-19\n", 59);
+	return (1);
+}
