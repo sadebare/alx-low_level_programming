8d81a000180c0842adabbae24c0230c5b6818f9d
Adebare <adebaresulaimon04@gmail.com>
2022-03-13 18:57:03 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		for(i = '0'; i <= '9'; i++)
+		for (i = '0'; i <= '9'; i++)
 		{
 			if (c < i)
 			{
 				putchar(c);
 				putchar(i);
 
-				if (c != 'g' || (c == '8' && i != '9'))
+				if (c != '8' || (c == '8' && i != '9'))
 				{
 					putchar(',');
 					putchar(' ');
