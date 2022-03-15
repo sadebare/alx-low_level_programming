d6e6304cf5061ccf179e1aa73e5fe7ad5925e5cb
adebare <adebaresulaimon04@gmail.com>
2022-03-15 14:53:01 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	for (h = 0; h < 24; h++)
+	for (h = 0; h <= 23; h++)
 	{
-		for (m = 0; h < 60; m++)
+		for (m = 0; h <= 59; m++)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
