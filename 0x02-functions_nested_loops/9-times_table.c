88639fb99999fc2293e07f3c86856bc1c7a80b70
adebare <adebaresulaimon04@gmail.com>
2022-03-16 01:57:42 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,12 +11,12 @@ void times_table(void)
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(48);
-		for(b = 1; b <= 9; b++)
+		for (b = 1; b <= 9; b++)
 		{
 			c = b * a;
 			_putchar(44);
 			_putchar(32);
-			if(c <= 9)
+			if (c <= 9)
 			{
 				_putchar(32);
 				_putchar(c + 48);
