7835656e43d74b8cac1c843755be27125bc7b4a8
adebare <adebaresulaimon04@gmail.com>
2022-03-16 02:25:12 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,17 +9,32 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n = 0; n < 98; n++)
+		while (n <= 98)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
 		}
+		
 	}
 	else if (n > 98)
 	{
-		for (n = 0; n > 98; n--)
+		while (n >= 98)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if(n != 98)
+			{
+				printf(", ");
+			}
+			n--;
 		}
 	}
-	printf("%d\n", n);
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
 }
