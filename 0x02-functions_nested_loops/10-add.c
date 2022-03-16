bd485159a4c2d7adc5e483ffa02017994c7c7042
adebare <adebaresulaimon04@gmail.com>
2022-03-15 17:01:13 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - A function that adds two integers
+ * @a: first perand
+ * @b: second operand
+ *
+ * Result: The sum of two parameters
+ */
+int add(int a, int b)
+{
+	add(a + b);
+}
