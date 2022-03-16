f88bd3d77dbe7a8d7b5b4a0994670f04f59cfb2f
adebare <adebaresulaimon04@gmail.com>
2022-03-16 08:05:50 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of the multiples of 3 amd 5 below 1024
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, sum = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			sum += a;
+		}
+		printf("%d\n", sum);
+	}
+}
