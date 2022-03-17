a967cbe80b9b949a0aae885be4bb6cbc21b33e68
adebare <adebaresulaimon04@gmail.com>
2022-03-17 04:43:11 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_numbers - function tat prints number from 0 to 9
  *
@@ -9,6 +10,8 @@ void print_numbers(void)
 	int a;
 
 	for (a = 48; a <= 58; a++)
+	{
 		_putchar(a);
+	}
 	_putchar('\n');
 }
