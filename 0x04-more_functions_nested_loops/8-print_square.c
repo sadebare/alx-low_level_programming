5c9e057a21f14bb8e912134a45332e67e7af5677
Shitttu Sulaimon Adebare <56326006+sadebare@users.noreply.github.com>
2022-03-17 12:22:58 -0700

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,18 +3,19 @@
  * print_square - function that prints a square followed by a new line
  * @size: parameter1
  *
- * Return: Always 0 (Success)
+ * Return: A square made with #
  */
 void print_square(int size)
 {
+	int i;
+	int j;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int i;
-		int j;
 
 		for (i = 1; i <= size; i++)
 		{
