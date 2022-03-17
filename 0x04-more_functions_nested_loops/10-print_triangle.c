cee27de61a736d313992f2734bf7644fe97355f6
adebare <adebaresulaimon04@gmail.com>
2022-03-17 12:49:44 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_triangle - function that prints a triangle, followed by a new line.
+ *@size: parameter1
+ *
+ *Return: # based on the parametrer inserted
+ */
+void print_triangle(int size)
+{
+	
+}
