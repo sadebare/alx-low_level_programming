bc2d2c027762e08798d165c451d53ccbfa11d88c
adebare <adebaresulaimon04@gmail.com>
2022-03-18 03:36:47 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * main - The prime factor of the number 612852475143
- *
+ *@void: no argument
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	long i, num = 612852475143;
+
 	for(i = 2; i <= num; i++)
 	{
 		if (num % i == 0)
