70535c45a5cda3840ff98bc15f02034a8c452df1
adebare <adebaresulaimon04@gmail.com>
2022-03-21 04:11:30 -0700

    function that return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+/**
+ * _strlen - function that returns the length of a string
+ * @s: parameter1
+ *
+ * Return: Always 0 (success)
+ */
+int _strlen(char *s)
+{
+	int length = strlen(*p);
+	return length;
+}
