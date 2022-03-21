079bf9ec2e4ebd3a79548a497b4dcecad1c2a317
adebare <adebaresulaimon04@gmail.com>
2022-03-21 03:52:46 -0700

    A function that swaps value

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+ * swap_int - A function that swaps two integers
+ * @a: parameter1
+ * @b: parameter2
+ *
+ * Return: Always success (0)
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
