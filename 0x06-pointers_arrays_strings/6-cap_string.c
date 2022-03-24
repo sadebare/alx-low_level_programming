67ee5da328c04e1ebb199db142d1238ca8f39868
adebare <adebaresulaimon04@gmail.com>
2022-03-24 04:30:47 -0700

    function that capitalize all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * cap_string - function that changes all lowercase letters to uppercase
+ * @s: parameter1
+ *
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+	int delt = 'a' - 'A';
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= 'a') && (s[i] <= 'z') && (s[i - 1] == ' ') || (s[i - 1] == '\t') || (s[i - 1] == '\n') || (s[i - 1] == ',') || (s[i - 1] == ';') || (s[i - 1] == '.') || (s[i - 1] == '!') || (s[i - 1] == '?') || (s[i - 1] == '"') || (s[i - 1] == '(') || (s[i - 1] == ')') || (s[i - 1] == '{') || (s[i - 1] == '}'))
+			s[i] -= delt;
+		i++;
+	}
+	return (s);
+}
