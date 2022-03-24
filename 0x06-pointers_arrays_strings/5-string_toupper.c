999e811fa09ed36a3c33a9c5b90bdb8260b1e016
adebare <adebaresulaimon04@gmail.com>
2022-03-24 03:45:31 -0700

    function that changes all lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - function that changes all lowercase letters to uppercase
+ * @str: parameter1
+ *
+ * Return: s
+ */
+char *string_toupper(char s*)
+{
+	int i;
+	int delt = 'a' - 'A';
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= 'a') && (s[i] <= 'z'))
+			s[i] -= delt;
+		i++;
+	}
+	return (s);
+}
