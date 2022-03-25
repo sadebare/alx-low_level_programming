633771bcdda8bf96defc51174ba1cbbd7ff0636a
Shitttu Sulaimon Adebare <56326006+sadebare@users.noreply.github.com>
2022-03-24 17:01:46 -0700

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,7 +9,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *n);
 char *leet(char *c);
-char *rot13(char *);
+char *rot13(char *s);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size); 
