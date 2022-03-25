a107f75364c0ba46cb3fdbdc0425ef28b6c12943
adebare <adebaresulaimon04@gmail.com>
2022-03-25 02:30:40 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,11 +1,26 @@
 #include "main.h"
 /**
- * rot13 - encodes a string in rot13
- * @s: string to be encoded
+ * rot13 - function that encodes a string using rot13
+ * @s: parameter1
  *
- * Return: the resulting string
+ * Return: s
  */
 char *rot13(char *s)
 {
-	
+	int i, j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; j++)
+	{
+		for (j = o; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
