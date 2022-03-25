634736e6b92a6351beb5ea01a1adc2a4103538bb
adebare <adebaresulaimon04@gmail.com>
2022-03-25 02:39:50 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,7 +9,6 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
