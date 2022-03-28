db45a29d2d1d72e0295fb5fed87d01cdbee97a32
adebare <adebaresulaimon04@gmail.com>
2022-03-28 05:42:12 -0700

    function that prints chess_board

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 
 #endif
