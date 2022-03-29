2eee97ba596e3a27ea402f8622f7d834294e5110
adebare <adebaresulaimon04@gmail.com>
2022-03-29 02:03:32 -0700

    correction

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int c, i;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c <= 9; c++)
 	{
-		for (i = '0'; i <= '9'; i++)
+		for (i = 0 + 1; i <= 9; i++)
 		{
 			if (c < i)
 			{
-				putchar(c);
-				putchar(i);
+				putchar(c + '0');
+				putchar(i + '0');
 
 				if (c != '8' || (c == '8' && i != '9'))
 				{
