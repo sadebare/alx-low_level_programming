8a951e586aeeac81234861f74df6331db414e803
adebare <adebaresulaimon04@gmail.com>
2022-04-01 03:02:48 -0700

    function that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A function that prints its name
+ * @argc: argument count
+ * @argv: Argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
