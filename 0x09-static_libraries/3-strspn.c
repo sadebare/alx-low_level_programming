83e39762b3b1246fbd2aa07c1662d273b488073d
adebare <adebaresulaimon04@gmail.com>
2022-04-02 01:05:32 +0100

    corrected

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strspn - function that gets te length of a prefix substring
+ * @s: param1
+ * @accept: param2
+ *
+ * Return: segment of s which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	unsigned int len;
+
+	len = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
+			;
+		if (s[i] == accept[j])
+			len++;
+		if (accept[j] == '\0')
+			return (len);
+	}
+	return (len);
+}
