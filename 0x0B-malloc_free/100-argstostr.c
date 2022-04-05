3cdd4e587a891e1cf379b26ac0b3b35ebde13dc3
adebare <adebaresulaimon04@gmail.com>
2022-04-05 13:06:04 +0100

    function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,37 +8,36 @@
  **/
 char *argstostr(int ac, char **av)
 {
-    char *strDup;
-    int i, j, k, size;
+	char *strDup;
+	int i, j, k, size;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
-    size = 0;
-    /* count the number of chars in each string */
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-            size++;
-        size++;
-    }
-    size++;
-    /* *
-     * allocate memory for total number of chars and
-     * new line for each word
-     */
-    strDup = malloc(sizeof(char) * size);
-    if (strDup == NULL)
-        return (NULL);
-    k = 0;
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-        {
-            strDup[k++] = av[i][j];
-        }
-        strDup[k++] = '\n';
-    }
-    strDup[k] = '\0';
-    
-    return (strDup);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	size = 0;
+/* count the number of chars in each string */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+		size++;
+	}
+	size++;
+/*
+ * allocate memory for total number of chars and
+ * new line for each word
+ */
+	strDup = malloc(sizeof(char) * size);
+	if (strDup == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			strDup[k++] = av[i][j];
+		}
+		strDup[k++] = '\n';
+	}
+	strDup[k] = '\0';
+	return (strDup);
 }
