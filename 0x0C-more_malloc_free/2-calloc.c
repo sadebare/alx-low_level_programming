212f4184a9dd36e7a95fe1421565de360ccd2e6d
adebare <adebaresulaimon04@gmail.com>
2022-04-07 15:37:46 +0100

    resolved

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,16 +10,16 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    char *memory;
-    unsigned int i;
+	char *memory;
+	unsigned int i;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-    memory = malloc(nmemb * size);
-    if (memory == NULL)
-        return (NULL);
-    for (i = 0; i < size * nmemb; i++)
-        *(memory + i) = 0;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	memory = malloc(nmemb * size);
+	if (memory == NULL)
+		return (NULL);
+	for (i = 0; i < size * nmemb; i++)
+		*(memory + i) = 0;
 
-    return ((void *)memory);
+	return ((void *)memory);
 }
