5d36fe1774b38ca306c1560318f783abc0b5231d
adebare <adebaresulaimon04@gmail.com>
2022-04-07 15:08:32 +0100

    function that allocates memory for an array, using malloc.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,32 +11,32 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
-	int j;
-	char *strDup;
+    int i;
+    unsigned int j;
+    char *strDup;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	i = 0;
-	while (s1[i] != '\0')
-		i++;
-	strDup = malloc(sizeof(char) * (i + n + 1));
-	if (strDup == NULL)
-		return (NULL);
-	i = j = 0;
-	while (s1[i] != '\0')
-	{
-		strDup[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		strDup[i] = s2[j];
-		i++;
-		j++;
-	}
-	strDup[i] = '\0';
-	return (strDup);
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+    i = 0;
+    while (s1[i] != '\0')
+        i++;
+    strDup = malloc(sizeof(char) * (n + i + 1));
+    if (strDup == NULL)
+        return (NULL);
+    i = j = 0;
+    while (s1[i] != '\0')
+    {
+        strDup[i] = s1[i];
+        i++;
+    }
+    while (s2[j] != '\0')
+    {
+        strDup[i] = s2[j];
+        i++;
+        j++;
+    }
+    strDup[i] = '\0';
+    return (strDup);
 }
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *_calloc - function that allocates memory for an array, using malloc.
+ * @nmemb: param1
+ * @size: param2
+ *
+ * Return: _calloc pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    char *memory;
+    unsigned int i;
+
+    if (nmemb == 0 || size == 0)
+        return (NULL);
+    memory = malloc(nmemb * size);
+    if (memory == NULL)
+        return (NULL);
+    for (i = 0; i < size * nmemb; i++)
+        memory[i] = 0;
+
+    return ((void *) memory);
+}
