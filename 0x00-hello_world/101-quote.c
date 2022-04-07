44e1ab58366c0743644c49acad91d7271d33ce02
adebare <adebaresulaimon04@gmail.com>
2022-04-08 00:37:57 +0100

    corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 
 /**
  * main - Entry point
- * Return - Always 1
+ *
+ * Return: 1
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpor, 2015-10-19");
+	printf("and that piece of art is useful\" - Dora Korpor, 2015-10-19\n");
 	return (1);
 }
