02f91748d573deeb56f9073c045d7088d747edb1
Shitttu Sulaimon Adebare <56326006+sadebare@users.noreply.github.com>
2022-04-08 00:31:33 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful
-			\" - Dora Korpor, 2015-10-19\n", 59);
+	printf("and that piece of art is useful\" - Dora Korpor, 2015-10-19\n");
 	return (1);
 }
