b76a62e4e7a03a236b7443e46ca7d6eea3026dd5
adebare <adebaresulaimon04@gmail.com>
2022-04-07 15:52:19 +0100

    resolved

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (arr == NULL)
 		return (NULL);
 
-	for (co = 0; c0 < i; co++)
+	for (co = 0; co < i; co++)
 		arr[co] = s1[co];
 	for (co_2 = 0; co < j; co_2++)
 	{
