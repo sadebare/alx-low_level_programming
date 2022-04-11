8a3c8c98a2205010ae26b452ac21d99457c44223
adebare <adebaresulaimon04@gmail.com>
2022-04-11 13:29:08 +0100

    C-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  function that prints the name of the file
+ *
+ * Return: Always success (0)
+ */
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
