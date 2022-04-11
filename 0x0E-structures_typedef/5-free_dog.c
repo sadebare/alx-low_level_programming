6888636d7111598897c50f4f95ad0a74196eccd8
adebare <adebaresulaimon04@gmail.com>
2022-04-11 16:55:31 +0100

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - free dogs
+ * @d: pointer to a dog of type dog_t
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
