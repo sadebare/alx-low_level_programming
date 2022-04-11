bdd08a5750c5f7651cce6ce68c34ba5ce13ca4e8
adebare <adebaresulaimon04@gmail.com>
2022-04-11 16:29:18 +0100

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *new_dog - Function that vreate a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: nothing
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
