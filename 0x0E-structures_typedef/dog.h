72f48a8ba38cec6c21b80f83aa978b5c0bb5cc8e
adebare <adebaresulaimon04@gmail.com>
2022-04-11 15:12:14 +0100

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef GUARDED
+#define GUARDED
+/**
+ * struct dog - describes a dog
+ * @name: first member
+ * @age: float age
+ * @owner: in character
+ *
+ * Description : Stuct of Dog that has its name, age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
