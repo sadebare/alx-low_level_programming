d58228ed2969aac2446a8c05cc6a73f725250cb1
adebare <adebaresulaimon04@gmail.com>
2022-04-14 14:29:10 +0100

    C - Variadic functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+/**
+ * print_all - function that prints anything.
+ * @format: types of arguments passed to the function
+ * 
+ */
+void print_all(const char * const format, ...)
+{
+    
+}
