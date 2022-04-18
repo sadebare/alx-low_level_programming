bfb02baa9a06fb00657052cefa1a12d1bf38f681
adebare <adebaresulaimon04@gmail.com>
2022-04-18 07:54:36 +0100

    reframe

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints the minimum number of coins to make change for a given amount
+ * main - prints the minimum number coin
  * @argc: arguement count
  * @argv: array of pointers to arguement strings
  * Return: number of coins or 1
