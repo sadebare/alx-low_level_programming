57926f8b8a341a6e8cbe36e9be2f49701446f505
adebare <adebaresulaimon04@gmail.com>
2022-04-22 12:25:26 +0100

    Added 100-first.c file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@
 void __attribute__((constructor)) calledFirst()
 {
     printf("You're beat! and yet, you must allow,\n");
-    printf("bore my house upon my back!\n");
-
+    printf("I bore my house upon my back!\n");
+    
 }
