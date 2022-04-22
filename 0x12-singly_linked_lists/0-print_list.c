b0ff6dde947c3970cfdfebe81d5c9fa59ec3f911
adebare <adebaresulaimon04@gmail.com>
2022-04-22 09:32:20 +0100

    corrected print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,8 @@
  */
 size_t print_list(const list_t *h)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,15 +16,15 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", 0, "(nil)");
 			h = h->next;
 			i++;
-		} else
+		}
+		else
 		{
-		printf("[%d] %s", h->len, h->str);
-		i++;
-		h = h->next;
-		printf("\n");
+			printf("[%d] %s\n", h->len, h->str);
+			i++;
+			h = h->next;
 		}
 	}
-	
-	return (i);
 
+	return (i);
 }
+
