be4e078059fb58815cb1ff100e32e7baae23665d
adebare <adebaresulaimon04@gmail.com>
2022-04-22 01:03:32 +0100

    Added a file that add node at the end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * *add_node_end - add a node at the end of the  lis_t list.
+ * @head: Address of first element of the list
+ *@str: pointer to string we want to print
+ * Return: the address of the new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+	struct list_s *newlist = NULL;
+	struct list_s *temp = NULL;
+	int i;
+
+	newlist = (struct list_s *)malloc(sizeof(struct list_s));
+	if (newlist == NULL)
+		return (NULL);
+	newlist->str = strdup(str);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	newlist->len = i;
+	newlist->next = NULL;
+	if (*head == NULL)
+	{
+		*head = newlist;
+	} else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = newlist;
+	}
+	return (newlist);
+
+}
