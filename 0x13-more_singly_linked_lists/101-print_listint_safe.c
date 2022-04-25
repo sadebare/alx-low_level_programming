4a80f91effc66c2af081e74abb80de07122eeb81
sadebare <adebaresulaimon04@gmail.com>
2022-04-25 15:36:49 -0400

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,10 +3,28 @@
  * print_listint_safe - function that print a listint_t
  * @head: pointer to the very first node
  *
- * Return: size
+ * Return: number of nodes in size_t or exit 98
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	head++;
-	return (0);
+	/* declarations */
+	int count = 0;
+	/* check for null pointer */
+	while (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		count += 1;
+
+		if (head > head->next)
+		{
+			head = head->next;
+		}
+		else
+		{
+			head = head->next;
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+	}
+	return (count);
 }
