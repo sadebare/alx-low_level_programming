31cf87a8864ed4357ddf809fbcce47f85b7780a2
sadebare <adebaresulaimon04@gmail.com>
2022-04-24 23:42:59 -0400

    More singly linked lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,11 +9,12 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	while (head != NULL)
+	while (head)
 	{
 		temp = head;
 		head = head->next;
 		free(temp);
 	}
-	free (head);
+
+	free(head);
 }
