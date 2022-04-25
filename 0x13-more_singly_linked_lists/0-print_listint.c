411fcaed9ca3823ee70dc38ea998c3962587a553
sadebare <adebaresulaimon04@gmail.com>
2022-04-24 23:29:59 -0400

    More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
- * print_listint - function that prints all elements
- * @h: pointer to a structure listint_s
- *
- * Return: number of nodes
- */
+  * print_listint - Prints all the elements of a linked list
+  * @h: The head of the linked list
+  *
+  * Return: The number of nodes of the linked list
+  */
 size_t print_listint(const listint_t *h)
 {
 	int i = 0;
