00b7189143ed857ba6292d16d69d4702c44f44a5
sadebare <adebaresulaimon04@gmail.com>
2022-04-24 23:22:15 -0400

    More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,14 @@ size_t print_listint(const listint_t *h)
 {
 	int i = 0;
 
-	while (h != NULL)
+	if (h != NULL)
 	{
+		while (h != NULL)
+		{
 			printf("%d\n", h->n);
 			i++;
 			h = h->next;
+		}
 	}
 	
 	return (i);
