16a553ddc67af2e3a45e78c1b28538013bff7adb
sadebare <adebaresulaimon04@gmail.com>
2022-04-25 15:28:40 -0400

    more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * print_listint_safe - function that print a listint_t
+ * @head: pointer to the very first node
+ *
+ * Return: size
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (0);
+}
