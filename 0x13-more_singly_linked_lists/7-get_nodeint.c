62434e221aa6037b734aabac0dbc6eaf026d6853
sadebare <adebaresulaimon04@gmail.com>
2022-04-26 10:34:17 -0400

    more singly linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - function that returns the nth node 
+ * get_nodeint_at_index - function that returns the nth node
  * @head: pointer to the first list in the linked list
- * @index: index node 
+ * @index: index node
  * Return: Address of index node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
