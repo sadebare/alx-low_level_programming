e0f40fea6703255907d99bcf1c9f0bdd806afecf
sadebare <adebaresulaimon04@gmail.com>
2022-04-26 10:38:58 -0400

    more singly linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		for (i = 0; i < (index - 1); i++)
 		{
 			if (temp->next)
-				temp = temp-> next;
+				temp = temp->next;
 			else
 				return (-1);
 		}
