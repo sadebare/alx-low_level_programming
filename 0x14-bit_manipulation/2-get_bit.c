801e59cc2682b003cded786a6830b1dcb6f59829
sadebare <adebaresulaimon04@gmail.com>
2022-04-28 14:42:01 -0400

    converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * get_bit - Function that returns the value of a bit at a given index.
+ * @n:number
+ * @index: Index, starting from 0 of the bit you want to get
+ * Return: The value of the bit at index index or -1 if an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+
+	if (index > 64)
+	{
+		return (-1);
+	}
+	else if ((n >> index) & 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
