63df4a8dccdd7b117825a8b1e515d4a20ce50ac4
sadebare <adebaresulaimon04@gmail.com>
2022-06-09 17:04:44 +0100

    function that returns the list length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,12 +6,12 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;
-        count++;
-    }
-   return (count);
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
