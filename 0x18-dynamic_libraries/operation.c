278408f32f2a0b5a3909283c1582364fcb7c78d2
sadebare <adebaresulaimon04@gmail.com>
2022-06-12 20:29:39 -0400

    adv task 1

---
+++ 0x18-dynamic_libraries/operation.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - functon that adds two integers
+ *
+ * Return: Addition of two integers
+ */
+
+int add(int a, int b)
+{
+    return (a+b);
+}
+
+/**
+ * sub - functon that substract two integers
+ *
+ * Return: Substraction of two integers
+ */
+
+int sub(int a, int b)
+{
+    return (a-b);
+}
+
+/**
+ * mul - functon that multiply two integers
+ *
+ * Return: Multiplication of two integers
+ */
+
+int mul(int a, int b)
+{
+    return (a*b);
+}
+
+/**
+ * div - functon that divides two integers
+ *
+ * Return: Division of two integers
+ */
+
+int div(int a, int b)
+{
+    return (a/b);
+}
+
+/**
+ * mod - functon that modulus two integers
+ *
+ * Return: modulus of two integers
+ */
+
+int mod(int a, int b)
+{
+    return (a%b);
+}
