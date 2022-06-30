9133ca70929c6f963ee5c85ea25b5382a3473c2a
sadebare <adebaresulaimon04@gmail.com>
2022-06-30 19:27:39 +0100

    Updated -adv task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,9 @@
  */
 int main(void)
 {
-
-	write(2, "and that piece of art is useful\" - Dora Korpor, 2015-10-19\n", 65);
+	write(2,
+		"and that piece of art is useful\" - Dora Korpor, 2015-10-19\n", 
+		65);
+		
 	return (1);
 }
