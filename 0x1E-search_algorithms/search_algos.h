c15e4cdf4222fc534eda0d7922dbf5c5eb511324
sadebare <adebaresulaimon04@gmail.com>
2022-10-05 08:53:46 +0100

    adv task

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,5 +13,8 @@ int jump_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int help_binary(int *array, int value, size_t lo, size_t hi);
 void array_print(int *array, size_t lo, size_t hi);
+int advanced_binary(int *array, size_t size, int value);
+int help_binary(int *array, int value, size_t lo, size_t hi);
+void array_print(int *array, size_t lo, size_t hi);
 
 #endif
