a1cb537a003782f82377503d55ec7af0d18d77c9
sadebare <adebaresulaimon04@gmail.com>
2022-10-05 09:03:05 +0100

    adv task

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,4 +1,7 @@
 #include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * binary_search - searches for a value in an array of
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,11 +10,7 @@ int binary_search(int *array, size_t size, int value);
 int help_binary(int *array, int value, size_t lo, size_t hi);
 void array_print(int *array, size_t lo, size_t hi);
 int jump_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int help_binary(int *array, int value, size_t lo, size_t hi);
-void array_print(int *array, size_t lo, size_t hi);
 int advanced_binary(int *array, size_t size, int value);
-int help_binary(int *array, int value, size_t lo, size_t hi);
-void array_print(int *array, size_t lo, size_t hi);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
